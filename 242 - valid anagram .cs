a81e69fbd4b4b5499f117b82a86f31e165a37431
user <malekaiter04@gmail.com>
2024-12-29 14:43:23 +0100

    new commit

---
+++ 242 - valid anagram .cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        if (s.Length != s.Length){
+            return false;
+        }
+
+        char[] sArray = s.ToCharArray();
+        char[] tArray = t.ToCharArray();
+        
+        Array.Sort(sArray);
+        Array.Sort(tArray);
+
+        return new string (sArray) == new string (tArray);
+    }
+}
