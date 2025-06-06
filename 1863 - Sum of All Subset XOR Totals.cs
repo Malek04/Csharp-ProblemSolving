299363d9318b1fa59be644408e4154201eaae99e
Malek Aiter <123811220+Malek04@users.noreply.github.com>
2025-06-06 20:20:40 +0100

    Update 1863 - Sum of All Subset XOR Totals.cs

--- 1863 - Sum of All Subset XOR Totals.cs
+++ 1863 - Sum of All Subset XOR Totals.cs
@@ -1,17 +1,10 @@
 public class Solution {
     public int SubsetXORSum(int[] nums) {
+        int result = 0;
         int n = nums.Length;
-        int count = 0;
-        for (int i = 1; i < (1 << n); i++) {
-            int currentXor = 0;
-            for (int j = 0; j < n; j++) {
-                if ((i & (1 << j)) != 0) {
-                    currentXor ^= nums[j];
-                }
-            }
-            count += currentXor;
+        foreach (int num in nums) {
+            result |= num;
         }
-        return count;
-
+        return result * (int)Math.Pow(2, n - 1);
     }
 }
